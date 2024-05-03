de9e1d7ee8f2212f31cc355fc551b30d85c3192c
Rbnra <rimebnr@hotmail.com>
2024-05-04 01:27:55 +0200

    modification du script js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,6 +6,10 @@ const getWorks = async()=> {
 try {
 const response = await fetch(`${API}`)
 const results = await response.json()
+for(let work of results) {
+const figure = createWorkFigure(work)
+galleryContainer.appendChild(figure)
+}
 console.log("Ensembles des works est",results)
 } catch(error){
     console.error("L'API est erroné",error)
@@ -13,3 +17,15 @@ console.log("Ensembles des works est",results)
 }
 getWorks()
 
+const createWorkFigure = (work) => {
+const figure = document.createElement("figure")
+const image = document.createElement("img")
+image.src = work.imageUrl
+image.alt = work.title
+figure.appendChild(image)
+
+const figcaption = document.createElement("figcaption")
+figcaption.textContent = work.title
+figure.appendChild(figcaption)
+return figure 
+}
